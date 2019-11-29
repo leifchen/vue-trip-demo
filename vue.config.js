7c28b0ac3f984494b40378fac98f40cb3eab218d
leifchen <leifchen90@gmail.com>
2019-11-29 21:50:45 +0800

    feat: add header

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+        .set('components', resolve('src/components'))
+        .set('styles', resolve('src/assets/styles'))
+  }
+}
